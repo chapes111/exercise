c149ebc46a8d3257e1009305e5386772a961fdf6
chapes111 <shawntasker@gmail.com>
2021-11-04 14:56:47 -0400

    Initial commit.

---
+++ exercise/Exercises.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace exercise
+{
+    public static class Exercises
+    {
+        public static object AboveBelow(int[] ints, int pivot)
+        {
+            int[] a = ints;
+            int p = pivot;
+
+            int abv = 0, bel = 0, len = a.Length;
+
+            for (int i = 0; i < len; ++i)
+            {
+                if (a[i] < p)
+                {
+                    ++bel;
+                }
+                else if (a[i] > p)
+                {
+                    ++abv;
+                }
+            }
+
+            var result = new { above = abv, below = bel };
+
+            return result;
+        }
+
+        public static string StringRotation(string original, int rotation)
+        {
+            char[] og = original.ToCharArray();
+            int len = original.Length, r = rotation;
+            char[] newArr = new char[len];
+
+            for (int i = 0; i < len; ++i)
+            {
+                newArr[i] = og[(len - r + i) % len];
+            }
+
+            string result = new string(newArr);
+
+            return result;
+        }
+    }
+}
---
+++ exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine(
+            //     Exercises.AboveBelow(
+            //         args[0].Trim('[', ']')
+	    //     					.Split(',')
+	    //     					.Select(int.Parse)
+	    //     				    .ToArray(),
+	    //     			int.Parse(args[1]))
+            // );
+
+            // Console.WriteLine(
+	    //     	    Exercises.StringRotation(
+	    //                 args[0],
+            //         int.Parse(args[1]))
+            //     );
+        }
+    }
+}
